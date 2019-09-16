ca0d4778c6dbd5195a243c6d4c79d57bbedcc0bc
belloadewalea@gmail.com <belloadewalea@gmail.com>
2019-09-16 12:51:10 +0100

    folder 2 video 7

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class Header extends Component{
+    render(){
+        return(
+            <header>
+            <div>Logo</div>
+            <input type="text"/>
+            </header>
+        )  
+    } 
+}
+
+ export default Header;
