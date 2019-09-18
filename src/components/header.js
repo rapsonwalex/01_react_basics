427319f1e9ce32633b8573480dd08c2d3f945ab9
belloadewalea@gmail.com <belloadewalea@gmail.com>
2019-09-18 17:37:08 +0100

    video 12 continue

--- src/components/header.js
+++ src/components/header.js
@@ -6,28 +6,24 @@ import '../css/styles.css'
 class Header extends Component{
 
     state = {
-        title: 'The keywords are:',
         keywords:''
     }
 
 
     inputChangeHandler = (event) => {
-        //console.log(event.target.value)
         this.setState({
             keywords: event.target.value
         })
     }
 
     render(){
-        console.log(this.state.keywords)
         return(
             <header>
             <div className="logo">Logo</div>
             <input type="text" 
             onChange={this.inputChangeHandler} 
             />
-            <div>{this.state.title}</div>
-            <div>{this.state.keywords}</div>
+     
             </header>
         )  
     } 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import JSON from './db.json';
+
 //COMPONENTS
 import Header from './components/header'
 
 const App = () =>{
+    console.log(JSON)
    return (
     <div>
         <Header/>
