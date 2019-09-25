f5a5ce5f2701c1ba316ae8866162be7788a4b13b
belloadewalea@gmail.com <belloadewalea@gmail.com>
2019-09-25 17:24:07 +0100

    completed react basics

--- src/components/header.js
+++ src/components/header.js
@@ -1,44 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
 import '../css/styles.css'
 
 
 
-class Header extends Component{
-
-    state = {
-        keywords:''
-    }
-
-
-    inputChangeHandler = (event) => {
-        this.setState({
-            keywords: event.target.value
-        })
-    }
-
-    render(){
-
-        // const style = {
-        //     background:'red'
-        // }
-
-        // if(this.state.keywords !==''){
-        //     style.background = 'blue'
-        // } else{
-        //     style.background = 'red'
-        // }
-
-
+const Header = (props)=>{
         return(
             <header>
             <div className="logo">Logo</div>
             <input type="text" 
-            onChange={this.inputChangeHandler} 
+            onChange={props.keywords} 
             />
      
             </header>
         )  
     } 
-}
 
  export default Header;
--- src/index.js
+++ src/index.js
@@ -9,17 +9,34 @@ import NewsList from './components/news_list';
 
 class App extends Component{
     state = {
-        news:JSON
+        news:JSON,
+        filtered:[]
     }
+
+getkeyword = (event) => {
+    //console.log(event.target.value)
+    let keyword = event.target.value;
+    let filtered = this.state.news.filter((item)=>{
+        return item.title.indexOf(keyword) > -1
+    });
+    this.setState({
+        filtered
+    })
+    // console.log(filtered)
+}
+
 render(){
-    console.log(this.state.news)
+
+    // console.log(this.state.news)
+    let newsFiltered = this.state.filtered;
+    let newsWhole = this.state.news
     return (
         <div>
-            <Header/>
-            <NewsList news={this.state.news}>
-                <h1>
+            <Header keywords={this.getkeyword} />
+            <NewsList news={newsFiltered.length === 0 ? newsWhole : newsFiltered}>
+                <h3>
                     The news are:
-                </h1>
+                </h3>
             </NewsList>
         </div>
         )
